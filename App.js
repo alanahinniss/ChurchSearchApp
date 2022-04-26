f312da855470f5cb7ca08c14763514246db684f0
MichaelMourelatos <michaelr@mourelatos.net>
2022-04-25 23:32:47 -0400

    First Edits

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div classname="container">
-        <h1>Welcome to Church Search!</h1>
+        <h1>Welcome to Church Search!!!!</h1>
         <img src = "church.jpeg" className ="App-logo" />
         <p>I just created my first React App</p>
       </div>
